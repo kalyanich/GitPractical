8678f04ed806933a801493eaf642090d110ae0b2
Kalyani Babasaheb Chaudhari <kalyanich@cybage.com>
2016-07-04 19:24:07 +0530

    Addition Operation

---
+++ src/Operations.java
@@ -0,0 +1,13 @@
+
+public class Operations {
+
+	public static void add(int a,int b)
+	{
+		System.out.println("Addition :: "+(a+b));
+	}
+	
+	public static void main(String[] args) {
+		add(2,3);
+	}
+
+}
